6af9d5787a54c89b69482e3477e107e6d8edade1
Alex Isaienko <thesonerik@gmail.com>
2023-03-11 03:49:06 +0200

    observable stream controllers in a weak map

--- lib/src/observable.dart
+++ lib/src/observable.dart
@@ -1,10 +1,28 @@
 import 'dart:async';
 
+import 'package:weak_map/weak_map.dart';
+
+final _observableControllers = WeakMap<Observable, StreamController>();
+
 abstract class Observable<T> {
   static ObservableValue<T> mutable<T>(T value) => ObservableValue._(value);
 
   T get value;
-  Stream<T> get stream;
+}
+
+extension ObservableStreamExtension<T> on Observable<T> {
+  Stream<T> get stream => _streamController.stream;
+}
+
+extension ObservableStreamControllerExtension<T> on Observable<T> {
+  StreamController<T> get _streamController {
+    var controller = _observableControllers[this] as StreamController<T>?;
+    if (controller == null) {
+      controller = StreamController<T>.broadcast();
+      _observableControllers[this] = controller;
+    }
+    return controller;
+  }
 }
 
 class ObservableValue<T> implements Observable<T> {
@@ -16,10 +34,6 @@ class ObservableValue<T> implements Observable<T> {
 
   set value(T newValue) {
     _value = newValue;
-    _controller.add(newValue);
+    _streamController.add(newValue);
   }
-
-  final _controller = StreamController<T>.broadcast();
-  @override
-  Stream<T> get stream => _controller.stream;
 }
