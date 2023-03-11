8b02ca13ddcf571a9b60ca8f5a7b770a453532ca
Alex Isaienko <thesonerik@gmail.com>
2023-03-11 19:47:03 +0200

    ObserverBuilder adjustments

--- lib/src/observer.dart
+++ lib/src/observer.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:easy_observable/easy_observable.dart';
 import 'package:flutter/widgets.dart';
 
-class ObserverBuilder extends StatefulWidget {
+class ObserverBuilder extends StatefulObserverWidget {
   const ObserverBuilder({
     Key? key,
     required this.builder,
@@ -16,20 +16,9 @@ class ObserverBuilder extends StatefulWidget {
 }
 
 class _ObserverBuilderState extends State<ObserverBuilder> {
-  StreamSubscription? _subscription;
-
-  @override
-  void dispose() {
-    _subscription?.cancel();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    _subscription?.cancel();
-    final computedWidget = Observable.computed(() => widget.builder(context));
-    _subscription = computedWidget.stream.listen((_) => setState(() {}));
-    return computedWidget.value;
+    return widget.builder(context);
   }
 }
 
