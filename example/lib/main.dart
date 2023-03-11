c02b581f5c9491e1b2d242325d19a60ef1e853f7
Alex Isaienko <thesonerik@gmail.com>
2023-03-11 06:26:56 +0200

    example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,13 @@
+import 'package:easy_observable/easy_observable.dart';
 import 'package:flutter/material.dart';
+
+class _State {
+  final counter1 = Observable.mutable(0);
+  final counter2 = Observable.mutable(0);
+  late final sum = Observable.computed(() => counter1.value + counter2.value);
+}
+
+final state = _State();
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +18,68 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('easy_observable example'),
+        ),
+        body: Column(
+          children: const [
+            _Counter1(),
+            _Counter2(),
+            _Sum(),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class _Counter1 extends StatelessWidget {
+  const _Counter1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ObserverBuilder(
+      builder: (context) => Row(
+        children: [
+          Text('Counter 1: ${state.counter1.value}'),
+          TextButton(
+            onPressed: () => state.counter1.value++,
+            child: const Text('Increment'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Counter2 extends StatelessWidget {
+  const _Counter2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ObserverBuilder(
+      builder: (context) => Row(
+        children: [
+          Text('Counter 2: ${state.counter2.value}'),
+          TextButton(
+            onPressed: () => state.counter2.value++,
+            child: const Text('Increment'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Sum extends StatelessWidget {
+  const _Sum({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ObserverBuilder(
+      builder: (context) => Text('Sum: ${state.sum.value}'),
     );
   }
 }
