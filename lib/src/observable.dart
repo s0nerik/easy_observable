6fa93691b4e6b8d9d5741c198835f79b5bcee805
Alex Isaienko <thesonerik@gmail.com>
2023-03-11 03:31:59 +0200

    ObservableValue

--- lib/src/observable.dart
+++ lib/src/observable.dart
@@ -1,6 +1,25 @@
 import 'dart:async';
 
 abstract class Observable<T> {
+  static ObservableValue<T> mutable<T>(T value) => ObservableValue._(value);
+
   T get value;
   Stream<T> get stream;
+}
+
+class ObservableValue<T> implements Observable<T> {
+  ObservableValue._(this._value);
+
+  T _value;
+  @override
+  T get value => _value;
+
+  set value(T newValue) {
+    _value = newValue;
+    _controller.add(newValue);
+  }
+
+  final _controller = StreamController<T>.broadcast();
+  @override
+  Stream<T> get stream => _controller.stream;
 }
