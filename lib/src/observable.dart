1866ed250f663ae61a8e11efe8518bebe96f4f6d
Alex Isaienko <thesonerik@gmail.com>
2023-03-11 04:34:45 +0200

    computed WIP

--- lib/src/observable.dart
+++ lib/src/observable.dart
@@ -3,9 +3,12 @@ import 'dart:async';
 import 'package:weak_map/weak_map.dart';
 
 final _observableChangeControllers = WeakMap<Observable, StreamController>();
+final _observableScopes = WeakMap<Observable, ObservableComputedScope>();
 
 abstract class Observable<T> {
   static ObservableValue<T> mutable<T>(T value) => ObservableValue._(value);
+  static ObservableComputedValue<T> computed<T>(T Function() valueGenerator) =>
+      ObservableComputedValue._(valueGenerator);
 
   T get value;
 }
@@ -36,4 +39,30 @@ class ObservableValue<T> implements Observable<T> {
     _value = newValue;
     _changesStreamController.add(newValue);
   }
+}
+
+class ObservableComputedValue<T> implements Observable<T> {
+  ObservableComputedValue._(this._compute) {
+    _value = scope.run(_compute);
+  }
+
+  final T Function() _compute;
+  final scope = ObservableComputedScope._();
+
+  late T _value;
+  @override
+  T get value => _value;
+}
+
+class ObservableComputedScope {
+  ObservableComputedScope._() {}
+
+  static const zoneKey = 'computed_scope';
+  static ObservableComputedScope? get current =>
+      Zone.current[ObservableComputedScope.zoneKey];
+
+  T run<T>(T Function() fn) =>
+      runZoned(fn, zoneValues: {ObservableComputedScope.zoneKey: this});
+
+  final _observables = <Observable>[];
 }
