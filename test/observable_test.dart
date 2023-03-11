325ed9e1f560d0489615bc21df0a3d3533238996
Alex Isaienko <thesonerik@gmail.com>
2023-03-11 05:59:21 +0200

    ObservableComputedValue test

--- test/observable_test.dart
+++ test/observable_test.dart
@@ -3,9 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('ObservableValue.stream notifies of value changes', () async {
-    var observable = Observable.mutable(0);
+    final observable = Observable.mutable(0);
 
-    var value = 0;
+    var value = observable.value;
     observable.stream.listen((observedValue) => value = observedValue);
 
     expect(value, 0);
@@ -21,5 +21,27 @@ void main() {
     observable.value = 5;
     await Future.value();
     expect(value, 5);
+  });
+  test(
+      'ObservableComputedValue.stream notifies of a computed value whenever any of the dependencies notify change',
+      () async {
+    final dep0 = Observable.mutable('a');
+    final dep1 = Observable.mutable(0);
+    final observable = Observable.computed(
+      () => '${dep0.value} ${dep1.value}',
+    );
+
+    var value = observable.value;
+    observable.stream.listen((observedValue) => value = observedValue);
+
+    expect(value, 'a 0');
+
+    dep0.value = 'b';
+    await Future.value();
+    expect(value, 'b 0');
+
+    dep1.value = 1;
+    await Future.value();
+    expect(value, 'b 1');
   });
 }
