7b5de6e26eaf39b4e7624993a60160c5f393361b
Alex Isaienko <thesonerik@gmail.com>
2023-03-20 02:49:12 +0200

    more tests

--- test/observable_test.dart
+++ test/observable_test.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:easy_observable/easy_observable.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -82,10 +83,10 @@ void main() {
     late MutableObservable<String> dep1;
     late MutableObservable<int> dep2;
     late MutableObservable<bool> dep3;
-    late Observable<String> computed1;
-    late Observable<String> computed2;
-    late Observable<String> computed3;
-    late Observable<String> combinedComputed;
+    late Observable<String> dep1computed;
+    late Observable<String> dep2computed;
+    late Observable<String> dep3computed;
+    late Observable<String> computed1and2;
     late Map<Observable, List> streamNotifications;
     late Map<Observable, int> streamComputations;
 
@@ -96,34 +97,37 @@ void main() {
       dep1 = Observable.mutable('a');
       dep2 = Observable.mutable(0);
       dep3 = Observable.mutable(false);
-      computed1 = Observable.computed(() {
+      dep1computed = Observable.computed(() {
         scheduleMicrotask(() {
-          streamComputations.putIfAbsent(computed1, () => 0);
-          streamComputations[computed1] = streamComputations[computed1]! + 1;
+          streamComputations.putIfAbsent(dep1computed, () => 0);
+          streamComputations[dep1computed] =
+              streamComputations[dep1computed]! + 1;
         });
-        return 'computed1: ${dep1.value}';
+        return 'dep1computed: ${dep1.value}';
       });
-      computed2 = Observable.computed(() {
+      dep2computed = Observable.computed(() {
         scheduleMicrotask(() {
-          streamComputations.putIfAbsent(computed2, () => 0);
-          streamComputations[computed2] = streamComputations[computed2]! + 1;
+          streamComputations.putIfAbsent(dep2computed, () => 0);
+          streamComputations[dep2computed] =
+              streamComputations[dep2computed]! + 1;
         });
-        return 'computed2: ${dep2.value}';
+        return 'dep2computed: ${dep2.value}';
       });
-      computed3 = Observable.computed(() {
+      dep3computed = Observable.computed(() {
         scheduleMicrotask(() {
-          streamComputations.putIfAbsent(computed3, () => 0);
-          streamComputations[computed3] = streamComputations[computed3]! + 1;
+          streamComputations.putIfAbsent(dep3computed, () => 0);
+          streamComputations[dep3computed] =
+              streamComputations[dep3computed]! + 1;
         });
-        return 'computed3: ${dep3.value}';
+        return 'dep3computed: ${dep3.value}';
       });
-      combinedComputed = Observable.computed(() {
+      computed1and2 = Observable.computed(() {
         scheduleMicrotask(() {
-          streamComputations.putIfAbsent(combinedComputed, () => 0);
-          streamComputations[combinedComputed] =
-              streamComputations[combinedComputed]! + 1;
+          streamComputations.putIfAbsent(computed1and2, () => 0);
+          streamComputations[computed1and2] =
+              streamComputations[computed1and2]! + 1;
         });
-        return '${computed1.value}, ${computed2.value}';
+        return '${dep1computed.value}, ${dep2computed.value}';
       });
 
       dep1.stream.listen(
@@ -135,22 +139,21 @@ void main() {
       dep3.stream.listen(
         (value) => streamNotifications.putIfAbsent(dep3, () => []).add(value),
       );
-      computed1.stream.listen(
+      dep1computed.stream.listen(
         (value) =>
-            streamNotifications.putIfAbsent(computed1, () => []).add(value),
+            streamNotifications.putIfAbsent(dep1computed, () => []).add(value),
       );
-      computed2.stream.listen(
+      dep2computed.stream.listen(
         (value) =>
-            streamNotifications.putIfAbsent(computed2, () => []).add(value),
+            streamNotifications.putIfAbsent(dep2computed, () => []).add(value),
       );
-      computed3.stream.listen(
+      dep3computed.stream.listen(
         (value) =>
-            streamNotifications.putIfAbsent(computed3, () => []).add(value),
+            streamNotifications.putIfAbsent(dep3computed, () => []).add(value),
       );
-      combinedComputed.stream.listen(
-        (value) => streamNotifications
-            .putIfAbsent(combinedComputed, () => [])
-            .add(value),
+      computed1and2.stream.listen(
+        (value) =>
+            streamNotifications.putIfAbsent(computed1and2, () => []).add(value),
       );
     });
 
@@ -163,12 +166,12 @@ void main() {
           expect(streamNotifications[dep1], ['b']);
           expect(streamNotifications[dep2], isNull);
           expect(streamNotifications[dep3], isNull);
-          expect(streamNotifications[computed1], ['computed1: b']);
-          expect(streamNotifications[computed2], isNull);
-          expect(streamNotifications[computed3], isNull);
+          expect(streamNotifications[dep1computed], ['dep1computed: b']);
+          expect(streamNotifications[dep2computed], isNull);
+          expect(streamNotifications[dep3computed], isNull);
           expect(
-            streamNotifications[combinedComputed],
-            ['computed1: b, computed2: 0'],
+            streamNotifications[computed1and2],
+            ['dep1computed: b, dep2computed: 0'],
           );
         },
       );
@@ -181,12 +184,12 @@ void main() {
           expect(streamNotifications[dep1], ['b']);
           expect(streamNotifications[dep2], isNull);
           expect(streamNotifications[dep3], [true]);
-          expect(streamNotifications[computed1], ['computed1: b']);
-          expect(streamNotifications[computed2], isNull);
-          expect(streamNotifications[computed3], ['computed3: true']);
+          expect(streamNotifications[dep1computed], ['dep1computed: b']);
+          expect(streamNotifications[dep2computed], isNull);
+          expect(streamNotifications[dep3computed], ['dep3computed: true']);
           expect(
-            streamNotifications[combinedComputed],
-            ['computed1: b, computed2: 0'],
+            streamNotifications[computed1and2],
+            ['dep1computed: b, dep2computed: 0'],
           );
         },
       );
@@ -200,43 +203,39 @@ void main() {
           await Future.value();
           expect(streamNotifications[dep1], ['b', 'b', 'b']);
           expect(
-            streamNotifications[computed1],
+            streamNotifications[dep1computed],
             [
-              'computed1: b',
-              'computed1: b',
-              'computed1: b',
+              'dep1computed: b',
+              'dep1computed: b',
+              'dep1computed: b',
             ],
           );
           expect(
-            streamNotifications[combinedComputed],
+            streamNotifications[computed1and2],
             [
-              'computed1: b, computed2: 0',
-              'computed1: b, computed2: 0',
-              'computed1: b, computed2: 0',
+              'dep1computed: b, dep2computed: 0',
+              'dep1computed: b, dep2computed: 0',
+              'dep1computed: b, dep2computed: 0',
             ],
           );
         },
       );
     });
+    group('Observer widget rules', () {
+      testWidgets(
+        'ObserverBuilder does not rebuild unnecessarily',
+        (widgetTester) async {
+          var rebuilds = 0;
+          final widget = ObserverBuilder(
+            builder: (context) {
+              computed1and2.value;
+              rebuilds++;
+              return const SizedBox.shrink();
+            },
+          );
+          await widgetTester.pumpWidget(widget);
+        },
+      );
+    });
   });
-  // group('Observer widget rules', () {
-  //   testWidgets(
-  //     'ObserverBuilder does not rebuild unnecessarily',
-  //     (widgetTester) {
-  //       final dep = Observable.mutable(0);
-  //       final computed = Observable.computed(() => dep.value);
-  //       final widget = ObserverBuilder(
-  //         builder: (context) {
-  //           return const SizedBox.shrink();
-  //         },
-  //       );
-  //       final observer = ObserverWidget(widget);
-  //       expect(observer.buildCount, 0);
-  //       expect(observer.buildCount, 0);
-  //       dep.value = 1;
-  //       expect(observer.buildCount, 0);
-  //       expect(observer.buildCount, 0);
-  //     },
-  //   );
-  // });
 }
