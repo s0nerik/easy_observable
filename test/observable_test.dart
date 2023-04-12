8b33734a12c638804ba159872b5033e30d7760a0
Alex Isaienko <thesonerik@gmail.com>
2023-04-12 17:32:26 +0300

    recomputation tests

--- test/observable_test.dart
+++ test/observable_test.dart
@@ -373,5 +373,53 @@ void main() {
       expect(computed.value, 'b1');
       expect(recomputations, 3);
     });
+
+    test('no longer referenced observables do_not trigger a recompute', () {
+      final obs1 = Observable.mutable('a');
+      final obs2 = Observable.mutable(0);
+
+      bool readObs1 = true;
+      bool readObs2 = true;
+      var recomputations = 0;
+      final computed = Observable.computed(() {
+        recomputations++;
+        if (readObs1 && readObs2) {
+          return '${obs1.value}${obs2.value}';
+        } else if (readObs1) {
+          return obs1.value;
+        } else if (readObs2) {
+          return '${obs2.value}';
+        } else {
+          return '';
+        }
+      });
+
+      expect(computed.value, 'a0');
+      expect(recomputations, 1);
+
+      obs1.value = 'b';
+      obs2.value = 1;
+      expect(computed.value, 'b1');
+      expect(recomputations, 3);
+
+      readObs1 = false;
+      obs1.value = 'c';
+      expect(computed.value, '1');
+      expect(recomputations, 4);
+
+      obs1.value = 'd';
+      expect(computed.value, '1');
+      expect(recomputations, 4);
+
+      readObs2 = false;
+      obs2.value = 2;
+      expect(computed.value, '');
+      expect(recomputations, 5);
+
+      obs1.value = 'e';
+      obs2.value = 3;
+      expect(computed.value, '');
+      expect(recomputations, 5);
+    });
   });
 }
