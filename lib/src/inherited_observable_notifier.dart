f10625c25b1569c25817db61df97f331f9ba22df
Alex Isaienko <thesonerik@gmail.com>
2023-06-20 02:14:23 +0300

    inherited_observable_notifier.dart: clear no-longer-needed subscriptions (only last one is always alive)

--- lib/src/inherited_observable_notifier.dart
+++ lib/src/inherited_observable_notifier.dart
@@ -24,6 +24,7 @@ class ObservableNotifierInheritedElement extends InheritedElement {
   ObservableNotifierInheritedElement(super.widget);
 
   final _elementSubs = <Element, Map<Observable, StreamSubscription>>{};
+  final _frameElementSubs = <Element, Map<Observable, StreamSubscription>>{};
 
   @override
   void mount(Element? parent, Object? newSlot) {
@@ -31,16 +32,47 @@ class ObservableNotifierInheritedElement extends InheritedElement {
     if (_frame.value == 0) {
       SchedulerBinding.instance.addPersistentFrameCallback(_onBeginFrame);
     }
+    SchedulerBinding.instance.addPostFrameCallback(_onPostFrame);
+  }
+
+  void _onPostFrame(_) {
+    if (!mounted) return;
+
+    // dispose all subscriptions that are no longer present in the frame
+    // subscriptions map, but only for elements that are present within the
+    // frame element subscriptions map
+    for (final element in _frameElementSubs.keys) {
+      final frameObservableSubs = _frameElementSubs[element]!;
+      final observableSubs = _elementSubs[element]!;
+      for (final observable in observableSubs.keys) {
+        if (!frameObservableSubs.containsKey(observable)) {
+          final sub = observableSubs[observable]!;
+          sub.cancel();
+        }
+      }
+      _elementSubs[element] = frameObservableSubs;
+      if (observableSubs.isEmpty) {
+        _elementSubs.remove(element);
+      }
+    }
+
+    _frameElementSubs.clear();
+    SchedulerBinding.instance.addPostFrameCallback(_onPostFrame);
   }
 
   @override
   void updateDependencies(Element dependent, Object? aspect) {
     final observable = aspect as Observable;
+
     _elementSubs[dependent] ??= <Observable, StreamSubscription>{};
     final observableSubs = _elementSubs[dependent]!;
     observableSubs[observable] ??= observable.stream.listen((_) {
       dependent.markNeedsBuild();
     });
+
+    _frameElementSubs[dependent] ??= <Observable, StreamSubscription>{};
+    final frameObservableSubs = _frameElementSubs[dependent]!;
+    frameObservableSubs[observable] = observableSubs[observable]!;
   }
 
   @override
