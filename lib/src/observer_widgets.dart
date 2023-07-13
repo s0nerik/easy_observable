6c4def3037e863658a51989e583850535b8db9e8
Alex Isaienko <thesonerik@gmail.com>
2023-07-13 23:13:40 +0300

    observer_widgets.dart update

--- lib/src/observer_widgets.dart
+++ lib/src/observer_widgets.dart
@@ -1,133 +1,18 @@
-import 'dart:async';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
-import 'observer_scope.dart';
+import 'inherited_observable_notifier.dart';
 
-class ObserverWidgetKey extends LocalKey {
-  const ObserverWidgetKey._(this.key);
-
-  final Key? key;
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is ObserverWidgetKey &&
-          runtimeType == other.runtimeType &&
-          key == other.key;
-
-  @override
-  int get hashCode => key.hashCode;
-}
-
-class ObserverBuilder extends ObserverStatelessWidget {
-  ObserverBuilder({
-    Key? key,
+class ObserverBuilder extends StatelessWidget {
+  const ObserverBuilder({
+    super.key,
     required this.builder,
-  }) : super(key: key);
+  });
 
   final WidgetBuilder builder;
 
   @override
   Widget build(BuildContext context) {
+    context.unwatchObservables();
     return builder(context);
-  }
-}
-
-abstract class ObserverStatelessWidget extends StatelessWidget {
-  ObserverStatelessWidget({Key? key}) : super(key: ObserverWidgetKey._(key));
-
-  @override
-  StatelessElement createElement() => ObserverStatelessElement(this);
-}
-
-class ObserverStatelessElement extends StatelessElement
-    with ObserverElementMixin {
-  ObserverStatelessElement(super.widget);
-}
-
-abstract class ObserverStatefulWidget extends StatefulWidget {
-  ObserverStatefulWidget({Key? key}) : super(key: ObserverWidgetKey._(key));
-
-  @override
-  StatefulElement createElement() => ObserverStatefulElement(this);
-}
-
-class ObserverStatefulElement extends StatefulElement
-    with ObserverElementMixin {
-  ObserverStatefulElement(super.widget);
-}
-
-mixin ObserverElementMixin on ComponentElement {
-  Widget? _childWidget;
-  bool _childWidgetBuildScheduled = false;
-
-  bool _selfBuildScheduled = false;
-
-  late final _build = kDebugMode ? _buildWithTypeErrorDebugging : super.build;
-  Widget _buildWithTypeErrorDebugging() {
-    try {
-      return super.build();
-    } on TypeError catch (e, stackTrace) {
-      final errorText = e.toString();
-      if (errorText.contains('_ComputedObservable') &&
-          errorText.contains('is not a subtype of type') &&
-          errorText.contains('MutableObservable')) {
-        FlutterError.onError?.call(FlutterErrorDetails(
-          exception: e,
-          stack: stackTrace,
-          library: 'easy_observable',
-          context: ErrorDescription('Error in ObserverWidget'),
-        ));
-        return ConstrainedBox(
-          constraints: BoxConstraints(
-            maxHeight: 20,
-            maxWidth: MediaQuery.of(this).size.width,
-          ),
-          child: ErrorWidget.withDetails(message: 'Hot restart is needed!'),
-        );
-      }
-      rethrow;
-    }
-  }
-
-  void _scheduleBuild() {
-    if (_childWidget == null) {
-      _childWidget = _build();
-      return;
-    }
-    if (!mounted) return;
-    scheduleMicrotask(_runScheduledBuild);
-    _childWidgetBuildScheduled = true;
-  }
-
-  void _runScheduledBuild() {
-    if (!mounted) return;
-    if (!_childWidgetBuildScheduled) return;
-    _childWidget = _build();
-    _childWidgetBuildScheduled = false;
-    markNeedsBuild();
-    _selfBuildScheduled = true;
-  }
-
-  ObserverScope? _observerScope;
-
-  @override
-  void unmount() {
-    _observerScope?.dispose();
-    _observerScope = null;
-    super.unmount();
-  }
-
-  @override
-  Widget build() {
-    if (!_selfBuildScheduled) {
-      _childWidget = null;
-      _observerScope?.dispose();
-      _observerScope = ObserverScope(_scheduleBuild);
-    }
-    _selfBuildScheduled = false;
-    return _childWidget!;
   }
 }
