0481af63c7c18650cf4ecefb9fe645350ea3669f
Alex Isaienko <thesonerik@gmail.com>
2023-07-16 21:11:47 +0300

    imports adjustment

--- lib/src/debug_logging.dart
+++ lib/src/debug_logging.dart
@@ -1,6 +1,5 @@
-import 'package:easy_observable/src/observable_ref_holder.dart';
-
 import 'observable/observable.dart';
+import 'observable_ref_holder.dart';
 import 'observer_notifier.dart';
 
 const _enableDebugLogging = false;
