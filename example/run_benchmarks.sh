cf28b55c4e84fedf7afb642d95ab757ec65bd4c0
Alex Isaienko <thesonerik@gmail.com>
2023-11-06 02:09:36 +0200

    updated run_benchmarks.sh to select macos target

--- example/run_benchmarks.sh
+++ example/run_benchmarks.sh
@@ -1,4 +1,4 @@
 # Prefer running these on mobile devices, as it seems like macOS and web don't support running integration tests
 # in profile mode good enough.
 
-flutter drive --profile --driver test_driver/perf_driver.dart --target integration_test/benchmarks/observer_widgets_bench.dart --no-dds
+fvm flutter drive --profile --driver test_driver/perf_driver.dart --target integration_test/benchmarks/observer_widgets_bench.dart --no-dds -d macos
